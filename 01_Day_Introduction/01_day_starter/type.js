d1e245abb8b45a3067026c7813a51323df0e8deb
yugao <gao.yu1@northeastern.edu>
2022-01-23 20:48:54 -0800

    update

---
+++ 01_Day_Introduction/01_day_starter/type.js
@@ -0,0 +1,4 @@
+let a = 'You cannot end a sentence with because because because is a conjunction';
+console.log(a.slice(a.indexOf('because'), a.lastIndexOf('because')));
+
+
--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,3 @@
-// this is your main.js script
+var str = 'banana';
+console.log(str.slice(1));
+console.log(str.split('\h'));
